2569218fcaa31af503cd21a2eb67f0c3ae54b77c
Hyeokjin Jin <hyeokjinjin@gmail.com>
2023-11-28 21:13:06 -0500

    Added LinkedList and LinkedListIterator

--- HW08/LinkedList.java
+++ HW08/LinkedList.java
@@ -6,27 +6,37 @@ public class LinkedList<T> implements List<T> {
     private Node<T> head;
     private int size;
 
-    // TODO JAVADOC
     public LinkedList() {
         head = null;
         size = 0;
     }
 
-    // TODO JAVADOC
     public LinkedList(T[] data) {
+        if (data == null) {
+            throw new IllegalArgumentException("Input array cannot be null");
+        }
         for (T datum : data) {
+            if (datum == null) {
+                throw new IllegalArgumentException("Input array cannot contain any null elements.");
+            }
             add(datum); // keep adding to end
         }
     }
 
-    // TODO JAVADOC
     public Node<T> getHead() {
         return head;
     }
 
-    // TODO JAVADOC
     public T[] toArray() {
-        return null; // FIXME
+        T[] arr = (T[]) new Object[size];
+        Node<T> currentNode = head;
+        int index = 0;
+
+        while (currentNode != null) {
+            arr[index++] = currentNode.getData();
+            currentNode = currentNode.getNext();
+        }
+        return arr;
     }
 
     @Override
@@ -55,32 +65,121 @@ public class LinkedList<T> implements List<T> {
 
     @Override
     public void add(T element) throws IllegalArgumentException {
-        // FIXME
+        if (element == null) {
+            throw new IllegalArgumentException("Element cannot be null.");
+        }
+
+        Node<T> newNode = new Node<>(element);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> currentNode = head;
+            while (currentNode.getNext() != null) {
+                currentNode = currentNode.getNext();
+            }
+            currentNode.setNext(newNode);
+        }
+        size++;
     }
 
     @Override
     public void add(int index, T element) throws IndexOutOfBoundsException, IllegalArgumentException {
-        // FIXME
+        if (index > size && element == null) {
+            throw new IndexOutOfBoundsException("Invalid index and element cannot be null.");
+        } else if (index > size) {
+            throw new IndexOutOfBoundsException("Invalid index.");
+        } else if (element == null) {
+            throw new IllegalArgumentException("Element cannot be null.");
+        }
+
+        int currentIndex = 0;
+        Node<T> currentNode = head;
+        while (currentIndex < index) {
+            currentNode = currentNode.getNext();
+            currentIndex++;
+        }
+
+        Node<T> newNode = new Node<>(element);
+        newNode.setNext(currentNode.getNext());
+        currentNode.setNext(newNode);
+        size++;
     }
 
     @Override
     public T remove() throws NoSuchElementException {
-        return null; // FIXME
+        if (isEmpty()) {
+            throw new NoSuchElementException("LinkedList is empty.");
+        }
+
+        T removedData = head.getData();
+        head = head.getNext();
+        size--;
+        return removedData;
     }
 
     @Override
     public T remove(int index) throws NoSuchElementException, IndexOutOfBoundsException {
-        return null; // FIXME
+        if (isEmpty() && index >= size) {
+            throw new NoSuchElementException("Index is invalid and list is empty.");
+        } else if (isEmpty()) {
+            throw new NoSuchElementException("LinkedList is empty.");
+        } else if (index >= size) {
+            throw new IndexOutOfBoundsException("Invalid Index.");
+        }
+
+        int currentIndex = 0;
+        Node<T> previousNode = head;
+        while (currentIndex < (index - 1)) {
+            previousNode = previousNode.getNext();
+            currentIndex++;
+        }
+
+        Node<T> removedNode = previousNode.getNext();
+        previousNode.setNext(removedNode.getNext());
+        size--;
+        return removedNode.getData();
     }
 
     @Override
     public T remove(T element) throws IllegalArgumentException, NoSuchElementException {
-        return null; // FIXME
+        if (element == null) {
+            throw new IllegalArgumentException("Argument element cannot be null.");
+        }
+
+        Node<T> currentNode = head;
+
+        while (currentNode.getNext() != null) {
+            if (currentNode.getData() == element) {
+                return currentNode.getData();
+            }
+            currentNode = currentNode.getNext();
+        }
+        throw new NoSuchElementException("Argument element not found in the list.");
     }
 
     @Override
     public T set(int index, T element) throws IndexOutOfBoundsException, IllegalArgumentException {
-        return null; // FIXME
+        if (element == null && index >= size) {
+            throw new IndexOutOfBoundsException("Element cannot be null and index is invalid");
+        } else if (element == null) {
+            throw new IllegalArgumentException("Element cannot be null.");
+        } else if (index >= size) {
+            throw new IndexOutOfBoundsException("Index is invalid");
+        }
+
+        int currentIndex = 0;
+        Node<T> currentNode = head;
+
+        while (currentIndex < index) {
+            currentNode = currentNode.getNext();
+            currentIndex++;
+        }
+
+        T replacedData = currentNode.getData();
+        currentNode.setData(element);
+
+        return replacedData;
     }
 
     @Override
@@ -95,17 +194,18 @@ public class LinkedList<T> implements List<T> {
 
     @Override
     public void clear() {
-        // FIXME
+        head = null;
+        size = 0;
     }
 
     @Override
     public boolean isEmpty() {
-        return false; // FIXME
+        return (size == 0);
     }
 
     @Override
     public int size() {
-        return 0; // FIXME
+        return size;
     }
 
     @Override
